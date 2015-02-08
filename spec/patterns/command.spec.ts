21329e6474ab14602d277c5114046accd7d4b31f
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2015-02-09 00:47:41 +0100

    Command implementation

---
+++ spec/patterns/command.spec.ts
@@ -0,0 +1,9 @@
+﻿/// <reference path="../../dist/good.d.ts" />
+/// <reference path="../libs/jasmine.d.ts" />
+describe("Command",() => {
+    "use strict";
+
+    it("must be defined",() => {
+        expect(Good.Patterns.Command).toBeDefined();
+    });
+});
---
+++ src/ts/patterns/command.ts
@@ -0,0 +1,29 @@
+﻿/**
+ * This module contains a basic Command pattern implementation.
+ */
+module Good.Patterns.Command {
+
+    /**
+     * A Command object is able to execute actions over some data.
+     */
+    export class Command<T> {
+        private _data: T;
+
+        /**
+         * @param data This is the data object whose methods will be executed.
+         */
+        constructor(data: T) {
+            this._data = data;
+        }
+
+        /**
+         * Executes a method with the given arguments.
+         * @param method The method name.
+         * @param args The arguments for the execution.
+         * @returns The execution result.
+         */
+        execute<Y>(method: string, ...args: any[]): Y {
+            return this._data[method].apply(this._data, args);
+        }
+    }
+}
