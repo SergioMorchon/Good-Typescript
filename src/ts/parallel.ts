76b4c3dad7524839ab2bccb1030a8235675d92cd
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2015-02-08 18:30:33 +0100

    Task await callback now requires an await result

--- src/ts/parallel.ts
+++ src/ts/parallel.ts
@@ -3,11 +3,11 @@ module Good.Patterns.Parallel {
 
     export class Task<TResult, TProgress, TException> {
 
-        private _callback: (...args: any[]) => TResult;
+        private _callback: (...args: any[]) => Future.IAwait<TResult, TProgress, TException>;
         private _async: Future.Async<TResult, TProgress, TException>;
         private _thisArg: any;
 
-        constructor(callback: (...args: any[]) => TResult, thisArg: any = null) {
+        constructor(callback: (...args: any[]) => Future.IAwait<TResult, TProgress, TException>, thisArg: any = null) {
             this._callback = callback;
             this._async = new Future.Async<TResult, TProgress, TException>();
             this._thisArg = thisArg;
@@ -20,7 +20,7 @@ module Good.Patterns.Parallel {
         run(...args: any[]): Future.IAwait<TResult, TProgress, TException> {
             setTimeout(() => {
                 try {
-                    this._async.resolve(this._callback.apply(this._thisArg, args));
+                    (<Future.IAwait<TResult, TProgress, TException>>this._callback.apply(this._thisArg, args)).done(this._async.resolve).fail(this._async.reject);
                 } catch (e) {
                     this._async.reject(e);
                 }
