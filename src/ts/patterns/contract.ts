9d0916853d949beb3b03b691a2013b9cd22475a8
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2015-02-10 12:38:45 +0100

    Improved Contract API
    Laxed valid type: truthy values instead of strictly booleans.

--- src/ts/patterns/contract.ts
+++ src/ts/patterns/contract.ts
@@ -16,8 +16,10 @@ module Good.Patterns.Contract {
     /**
      * You can express a pre-condition by requiring a valid value. If not, will throw an error.
      * This line should be at the top of a function body to ensure a correct start state.
+     * @param valid A value that must be truthy.
+     * @param error An error message.
      */
-    export function requires(valid: boolean, error: string = "Contract pre-condition violation") {
+    export function requires(valid: any, error: string = "Contract pre-condition violation") {
         if (!valid) {
             throw new PreConditionError(error);
         }
@@ -26,8 +28,10 @@ module Good.Patterns.Contract {
     /**
      * You can express a post-condition by requiring a valid value. If not, will throw an error.
      * This line should be at the bottom of a function body to ensure a correct end state.
+     * @param valid A value that must be truthy.
+     * @param error An error message.
      */
-    export function ensures(valid: boolean, error: string = "Contract post-condition violation") {
+    export function ensures(valid: any, error: string = "Contract post-condition violation") {
         if (!valid) {
             throw new PostConditionError(error);
         }
